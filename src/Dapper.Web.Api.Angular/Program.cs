b0b0b11d2ac6c097cff71e76cc02419ddfaee745
andy <andy2903.alp@gmail.com>
2018-10-02 20:12:21 -0300

    Inserindo docker file

--- src/Dapper.Web.Api.Angular/Program.cs
+++ src/Dapper.Web.Api.Angular/Program.cs
@@ -12,6 +12,7 @@ namespace Dapper.Web.Api.Angular
 {
     public class Program
     {
+       
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
